1a4148f13243409d5e4230fc110d827e998e0c73
JoeLee <lizhou1021@qq.com>
2023-08-20 23:25:45 +0800

    feat: 优化积雪效果

--- packages/app/src/engine/babylonjs/SnowCoverMaterialPlugin.ts
+++ packages/app/src/engine/babylonjs/SnowCoverMaterialPlugin.ts
@@ -3,7 +3,7 @@ import { Vector3 } from "@babylonjs/core/Maths/math.vector";
 import { Color3 } from "@babylonjs/core/Maths/math.color";
 
 
-import imgUrl from '../../assets/southeast.jpg'
+// import imgUrl from '../../assets/southeast.jpg'
 import { MaterialPluginBase } from "@babylonjs/core/Materials/materialPluginBase";
 import { Texture, UniformBuffer } from "@babylonjs/core/Materials";
 
@@ -11,13 +11,15 @@ import { Texture, UniformBuffer } from "@babylonjs/core/Materials";
 export class SnowCoverMaterialPlugin extends MaterialPluginBase {
   public snowAmount = 0.5;
   public smoothFactor = 0.5
-  private texture;
+  private texture: Texture;
+  private norTexture: Texture;
   private _isEnabled = false;
 
   constructor(material) {
     // last parameter is a priority, which lets you define the order multiple plugins are run.
     super(material, "SnowCover", 200, { SNOWCOVER: false });
-    this.texture = new Texture(imgUrl, material.getScene())
+    this.texture = new Texture("/textures/snow/Snow_003_COLOR.jpg", material.getScene())
+    this.norTexture = new Texture("/textures/snow/Snow_003_NORM.jpg", material.getScene())
   }
 
   get isEnabled() {
@@ -51,7 +53,8 @@ export class SnowCoverMaterialPlugin extends MaterialPluginBase {
         { name: "smoothFactor", size: 1, type: "float" },
         { name: "snowColor", size: 3, type: "vec3" },
         { name: "snowDirection", size: 3, type: "vec3" },
-        { name: "snowTexture", size: 3, type: "vec3" },
+        // { name: "snowTexture", size: 3, type: "vec3" },
+        // { name: "snowNorTexture", size: 3, type: "vec3" },
       ],
       // now, on the fragment shader, add the uniform itself in case uniform buffers are not supported by the engine
       fragment: `#ifdef SNOWCOVER
@@ -63,13 +66,16 @@ export class SnowCoverMaterialPlugin extends MaterialPluginBase {
     };
   }
   getSamplers(samplers) {
-    samplers.push("SnowTexture");
+    samplers.push("snowTexture");
   }
   bindForSubMesh(uniformBuffer: UniformBuffer, scene, engine, subMesh) {
-    uniformBuffer.updateFloat("snowAmount", this.snowAmount);
-    uniformBuffer.updateColor3("snowColor", Color3.White());
-    uniformBuffer.updateVector3("snowDirection", new Vector3(0, 1));
-    uniformBuffer.setTexture("SnowTexture", this.texture);
+    if (this._isEnabled) {
+      uniformBuffer.updateFloat("snowAmount", this.snowAmount);
+      uniformBuffer.updateColor3("snowColor", Color3.White());
+      uniformBuffer.updateVector3("snowDirection", new Vector3(0, 1));
+      uniformBuffer.setTexture("snowTexture", this.texture);
+      uniformBuffer.setTexture("snowNorTexture", this.norTexture);
+    }
   }
 
   getCustomCode(shaderType) {
@@ -79,12 +85,20 @@ export class SnowCoverMaterialPlugin extends MaterialPluginBase {
         CUSTOM_FRAGMENT_DEFINITIONS: `
         varying vec3 vNormal;
         varying vec2 vUv;
-        uniform sampler2D SnowTexture;
+        uniform sampler2D snowTexture;
+        uniform sampler2D snowNorTexture;
+        `,
+        CUSTOM_FRAGMENT_BEFORE_LIGHTS: `
+        #ifdef SNOWCOVER
+          vec4 norSnowC=texture2D(snowNorTexture,vUv);
+          // normalW =normalW * (norSnowC.rgb*0.5+0.5);
+        
         `,
         CUSTOM_FRAGMENT_MAIN_END: `
             #ifdef SNOWCOVER
         
-            vec4 snowC=texture2D(SnowTexture,vUv);
+            vec4 snowC=texture2D(snowTexture,vUv);
+
 
             float an=dot(vNormal, snowDirection);
 
@@ -94,12 +108,15 @@ export class SnowCoverMaterialPlugin extends MaterialPluginBase {
              // 使用平滑因子对积雪边缘进行平滑
             float n = smoothstep(0.0,smoothFactor, cover);
           
-            if (cover > 0.0) {
-               vec3 color = mix(vec3(1.0, 1.0, 1.0), snowC.rgb, n);
-              // vec3 color=mix(vec3(1.0,1.0,1.0),vec3(0.0,0.0,0.0),cover);
-              // gl_FragColor = vec4(snowC.rgb,clamp(an,0.0,1.0)); 
-               gl_FragColor = vec4(color,cover);
-            }
+            // if (cover > 0.0) {
+            //    vec3 color = mix(vec3(1.0, 1.0, 1.0), snowC.rgb, n);
+            //   // vec3 color=mix(vec3(1.0,1.0,1.0),vec3(0.0,0.0,0.0),cover);
+            //   // gl_FragColor = vec4(snowC.rgb,clamp(an,0.0,1.0)); 
+            //    gl_FragColor = snowC;
+            // }
+
+            gl_FragColor = mix(color,snowC,clamp(cover+.4,0.0,1.0));
+
             #endif
         `,
       };
